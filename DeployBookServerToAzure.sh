259a75c8dc8c99cb52db04fc50c1928d8f75ebcc
Jason DeBoever <github@deboever.us>
2019-01-17 14:50:53 -0500

    bash changes

--- DeployBookServerToAzure.sh
+++ DeployBookServerToAzure.sh
@@ -2,38 +2,27 @@
 
 # Replace the following URL with a public GitHub repo URL
 gitrepo=https://github.com/jasallen/RestModuleExperiments
-gitpath=RestModuleExperiments/BookServer/BookServer/BookServer.csproj
+gitpath=BookServer/BookServer/BookServer.csproj
 webappname=bookserver$RANDOM
 
 # Create a resource group.
-az group create --location westus --name MyXam150ResourceGrp
+az group create --location westus --name MyXam150ResourceGroup
 
 # Create an App Service plan in STANDARD tier (minimum required by deployment slots).
-az appservice plan create --name $webappname --resource-group MyXam150ResourceGrp --sku S1
+az appservice plan create --name $webappname --resource-group MyXam150ResourceGroup --sku S1
 
 # Create a web app.
-az webapp create --name $webappname --resource-group MyXam150ResourceGrp \
+az webapp create --name $webappname --resource-group MyXam150ResourceGroup \
 --plan $webappname
 
-az webapp config appsettings set -g MyXam150ResourceGrp -n $webappname --settings PROJECT=$gitpath
+az webapp config appsettings set -g MyXam150ResourceGroup -n $webappname --settings PROJECT=$gitpath
 
-#Create a deployment slot with the name "staging".
-az webapp deployment slot create --name $webappname --resource-group MyXam150ResourceGrp \
---slot staging
-
-# Deploy sample code to "staging" slot from GitHub.
-az webapp deployment source config --name $webappname --resource-group MyXam150ResourceGrp \
---slot staging --repo-url $gitrepo --branch master --manual-integration
-
-# Copy the result of the following command into a browser to see the staging slot.
-echo http://$webappname-staging.azurewebsites.net
-
-# Swap the verified/warmed up staging slot into production.
-az webapp deployment slot swap --name $webappname --resource-group MyXam150ResourceGrp \
---slot staging
+# Deploy sample code from GitHub.
+az webapp deployment source config --name $webappname --resource-group MyXam150ResourceGroup \
+--repo-url $gitrepo --branch master --manual-integration
 
 # Copy the result of the following command into a browser to see the web app in the production slot.
 echo http://$webappname.azurewebsites.net
 
 # Delete the Resources
-# az group delete --name myResourceGroup
+# az group delete --name MyXam150ResourceGroup
